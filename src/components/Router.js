728bb8464fca61192eaa8acf978c779e0f94a9ff
Emilia <emilia.mena26@gmail.com>
2020-12-12 11:17:10 -0500

    login and signup routes added

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import About from './About';
+import Login from './Login';
+import Signup from './Signup';
 import Goals from './Goals';
 import Passport from './Paspport';
 import NotFound from './NotFound';
@@ -11,9 +13,11 @@ const Router = () => (
     <BrowserRouter>
     <Switch>
         <Route exact path="/" component={About}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/signup" component={Signup}/>
         <Route exact path="/goals" component={Goals}/>
+         {/* <Route path="/goals/:goalId" component={} /> */}
         <Route exact path="/passport" component={Passport}/>
-        {/* <Route path="/store/:storeId" component={} /> */}
         <Route component={NotFound} />
     </Switch>
     </BrowserRouter>
