8c96c90de0a7b6626961e695a097b661aee8fc99
Emilia <emilia.mena26@gmail.com>
2020-12-12 11:05:10 -0500

    adding routing...goals,passport, about, notfound

---
+++ src/components/Paspport.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Paspport() {
+    return (
+        <div>
+            This is the passport page
+        </div>
+    )
+}
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import About from './About';
+import Goals from './Goals';
+import Passport from './Paspport';
 import NotFound from './NotFound';
+
+
 
 const Router = () => (
     <BrowserRouter>
     <Switch>
-        {/* <Route exact path="/" component={}/> */}
+        <Route exact path="/" component={About}/>
+        <Route exact path="/goals" component={Goals}/>
+        <Route exact path="/passport" component={Passport}/>
         {/* <Route path="/store/:storeId" component={} /> */}
         <Route component={NotFound} />
     </Switch>
