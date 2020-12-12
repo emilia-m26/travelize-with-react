38aadefed03ec31cee16365ae081e88606235071
Emilia <emilia.mena26@gmail.com>
2020-12-12 10:57:08 -0500

    starting routing for app, added dependencies needed for Router to work

---
+++ src/components/Router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NotFound from './NotFound';
+
+const Router = () => (
+    <BrowserRouter>
+    <Switch>
+        {/* <Route exact path="/" component={}/> */}
+        {/* <Route path="/store/:storeId" component={} /> */}
+        <Route component={NotFound} />
+    </Switch>
+    </BrowserRouter>
+
+)
+
+export default Router;
