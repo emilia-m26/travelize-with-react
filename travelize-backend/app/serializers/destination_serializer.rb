d0a69e2c7da65991ccef94b77d1e30b692c44797
Emilia <emilia.mena26@gmail.com>
2020-12-15 18:12:40 -0500

    Small adj on destination serializer

--- travelize-backend/app/serializers/destination_serializer.rb
+++ travelize-backend/app/serializers/destination_serializer.rb
@@ -1,5 +1,5 @@
 class DestinationSerializer < ActiveModel::Serializer
-  attributes :id, :goal_id, :location, :description, :date_traveled, :completed
+  attributes :id, :location, :description, :date_traveled, :completed
 
   belongs_to :goal
 end
