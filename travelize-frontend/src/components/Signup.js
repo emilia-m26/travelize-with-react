1b4d2f9347ae29d2342926fca11651707c0415f4
Emilia <emilia.mena26@gmail.com>
2020-12-28 10:24:09 -0500

    minor edit

--- travelize-frontend/src/components/Signup.js
+++ travelize-frontend/src/components/Signup.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Signup() {
     return (
         <div>
-            this is the signin page
+            this is the signup page
         </div>
     )
 }
